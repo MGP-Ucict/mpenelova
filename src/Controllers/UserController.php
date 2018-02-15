a914a17736b653af4cdae458a09b2f1ef524707b
mpenelova <mpenelova@ucc.uni-sofia.bg>
2018-02-15 16:41:15 +0200

    Change

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -33,7 +33,7 @@ public function store(UserRequest $request){
 		$userObj->email = $input['email'];
 		$userObj->password = bcrypt($input['password']);
 		$roles = $input['roles'];
-		$userObj->is_active = (isset($input['is_active']))?$input['is_active']:0;
+		//$userObj->is_active = (isset($input['is_active']))?$input['is_active']:0;
 		$userObj->save();
 		foreach($roles as $key=>$value){
 			$rr = Role::find($value);
@@ -84,7 +84,7 @@ public function update(UserUpdateRequest $request, $userId){
 		$userObj->email = $input['email'];
 		$userObj->password = bcrypt($input['password']);
 		$roles1 = $input['roles'];
-		$userObj->is_active = (isset($input['is_active']))?true:false;
+		//$userObj->is_active = (isset($input['is_active']))?true:false;
 		
 		foreach($roles0 as $key=>$value){
 			$rr = Role::find($value);
@@ -127,7 +127,7 @@ public function userDelete(Request $request, $userId){
 	
 	}
 	$userObjs = User::all();
-	 return View::make('user_list')->with(array('userObjs'=>$userObjs));
+	 return View::make('laravelroles/rolespermissions/user_list')->with(array('userObjs'=>$userObjs));
 
 }
 public function userList(Request $request){
