5410ff0b3f73ca03d8f611649b806bb57f7ec6db
mpenelova <mpenelova@ucc.uni-sofia.bg>
2018-02-15 11:55:39 +0200

    User Create

--- src/views/user_create.blade.php
+++ src/views/user_create.blade.php
@@ -8,7 +8,7 @@
 
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">{{trans('blah::translation.CreateUser')}}</div>
+                <div class="panel-heading"><h1>{{trans('blah::translation.CreateUser')}}</h1></div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -26,24 +26,62 @@
 			    </div>
 		    @endif
                     
-                    You are logged in!
+                    
                     {{ Form::open(['url' => 'user_create', 'method' => 'post']) }}
+                    <div class ="row  col-md-offset-1">
+                   	<div class = "col-md-4">	
 					<label>{{trans('blah::translation.Name')}}:</label>
+				    </div>
+				    <div class = "col-md-4">
 					{{ Form::text("name") }}
-					<br/>
+					</div>
+					</div>
+					<div class ="row  col-md-offset-1">
+                   	<div class = "col-md-4">
 					<label>{{trans('blah::translation.Email')}}:</label>
+				    </div>
+				    <div class = "col-md-4">
 					{{ Form::text("email") }}
-					<br/>
+					</div>
+					</div>
+					
+					<div class ="row  col-md-offset-1">
+                   	<div class = "col-md-4">	
 					<label>{{trans('blah::translation.Password')}}:</label>
+					</div>
+                   	<div class = "col-md-4">
 					{{ Form::password("password") }}
-					<br/>
+					</div>
+					</div>
+					<div class ="row  col-md-offset-1">
+                   	<div class = "col-md-4">	
 					<label>Is active:</label>
+					</div>
+					<div class = "col-md-4">	
 					{{ Form::checkbox("is_active", 1) }}
-					<br>
+					</div>
+				    </div>
+				    <div class ="row  col-md-offset-1">
+                   	<div class = "col-md-4">	
+					<label>{{trans('blah::translation.Roles')}}:</label>
+				    </div>
+					</div>
 					@foreach($roles as $roleObj)
+					<div class ="row  col-md-offset-2">
 						<label>{{ Form::checkbox("roles[]", $roleObj->id) }}	</label>				
 						{{$roleObj->name}}
-						<br>
+					</div>
 					@endforeach	
-				 {{ Form::submit('click me', ['name' => 'submit']) }}
+					<div class ="row  col-md-offset-1">
+					<div class = "col-md-4 col-md-offset-4">
+				 {{ Form::submit('Save', ['name' => 'submit']) }}
+				 </div>
+			</div>
                     {{ Form::close() }}
+
+                    </div>
+	       </div>
+        </div>
+    </div>
+
+@endsection
