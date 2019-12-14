e794e0ae6ea8e4af2e1a38da5b49708ed43b26dd
MGP-Ucict <penelova.maria@gmail.com>
2019-12-14 08:51:34 -0800

    Change

--- src/views/Routes/route_update.blade.php
+++ src/views/Routes/route_update.blade.php
@@ -3,48 +3,34 @@
 @section('content')
 <div class="container">
 @include('laravelroles.rolespermissions.header')
-
-    <div class="row">
-
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h1>{{trans('lang::translation.UpdateRoute')}}</h1></div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
-                    
-                   
-                    {{ Form::open(['url' => 'route_update/'.$routeId, 'method' => 'post']) }}
-                    <div class ="row  col-md-offset-1">
-                    <div class = "col-md-4">
-					<label>{{trans('lang::translation.Name')}}:</label>
-                    </div>
-                    <div class = "col-md-4">
-					{{ Form::text("name",$routeObj->name) }}
-					</div>
-                    </div>
-                    <div class ="row  col-md-offset-1">
-                    <div class = "col-md-4">
-					<label>{{trans('lang::translation.Route')}}:</label>
-                    </div>
-                    <div class = "col-md-4">
-					{{ Form::text("route",$routeObj->route) }}
-					</div>
-                    </div>
-					<div class ="row  col-md-offset-1">
-                    <div class = "col-md-4 col-md-offset-4">
-				 {{ Form::submit(trans('lang::translation.Save'), ['name' => 'submit']) }}
-                    {{ Form::close() }}
-                </div>
-            </div>
+<div class="panel panel-default">
+	<div class="panel-heading"><h1>{{trans('lang::translation.UpdateRoute')}}</h1></div>
+		<div class="panel-body">
+			@if (session('status'))
+				<div class="alert alert-success">
+					{{ session('status') }}
+				</div>
+			@endif 
+			{{ Form::open(['url' => 'route_update/'.$routeId, 'method' => 'post']) }}
+			<div class="form-group">
+				<label for="name">{{trans('lang::translation.Name')}}:</label>
+				{{ Form::text("name",$routeObj->name, ['class' => 'form-control']) }}
+			</div>
+			<div class="form-group">
+				<label for="route">{{trans('lang::translation.Route')}}:</label>
+				{{ Form::text("route",$routeObj->route, ['class' => 'form-control']) }}
+			</div>
+			<div class="form-group">
+				<label for="route">{{trans('lang::translation.Method')}}:</label>
+					{!! Form::select('method', array(
+						'GET','POST','PUT', 'DELETE'
+					), "GET", ['class' => 'form-control'] )!!}
+			</div>
+			<div class="form-group">
+				{!! Form::submit(trans('lang::translation.Save'), ['name' => 'submit','class' => 'btn btn-primary']) !!}
+				{{ Form::close() }}
+			</div>
 		</div>
-	    </div>
-         </div>
-    </div>
-</div>
+	</div>
+ </div>
 @endsection
--- src/views/header.blade.php
+++ src/views/header.blade.php
@@ -1,8 +1,6 @@
-
 <div class="navbar">
     <div class="navbar-inner">
-
-        <ul class="nav nav-tabs">
+		<ul class="nav nav-tabs">
 		@if(Auth::user()->hasAccess(['role_list']))
             <li><a href="/role_list">{{trans('lang::translation.Roles') }}</a></li>&nbsp;
 		@endif
@@ -12,5 +10,7 @@
 		@if(Auth::user()->hasAccess(['user_list']))
             <li><a href="/user_list">{{trans('lang::translation.Users') }}</a></li>&nbsp;
 		@endcan
+		</ul>
+	</div>
 </div>
 
